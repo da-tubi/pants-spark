d454a29fd0e792908326cc5d86dae3308cad87ec
Darcy Shen <da@tubi.tv>
2022-08-24 12:06:38 +0800

    Apache Spark version

--- src/jvm/tubi/common/package.scala
+++ src/jvm/tubi/common/package.scala
@@ -1,7 +1,15 @@
 package tubi
 
+import org.apache.spark.sql.SparkSession
+
 package object common {
   def greet(who: String, what: String): Unit = {
-    println(f"I am a ${who}'s ${what} with PySpark {pyspark.__version__}")
+    val spark = SparkSession
+      .builder()
+      .master("local[1]")
+      .appName("OnlyForVersion")
+      .getOrCreate()
+    println(s"I am a ${who}'s ${what} with Apache Spark ${spark.version}")
+    spark.stop()
   }
 }
